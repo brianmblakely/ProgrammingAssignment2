90769f101df66d196c7a2ec0b00ea3f5093110d4
brianmblakely <brianmblakely@gmail.com>
2015-10-21 20:06:18 -0500

    second update: improved comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,9 @@
-## Put comments here that give an overall description of what your function does
 
-##  The two functions makeCacheMatrix and Cache Solve are used to create a special object that stores a numeric matrix and caches its inverse.
 
-## Write a short comment describing this function
-## x: the input needs to be a square invertible matrix
+##  The two functions makeCacheMatrix and CacheSolve are used to create a special object that stores a numeric matrix and caches its inverse.
+
+## Function Description: makeCacheMatrix
+
 ## The first function, makeCacheMatrix creates a special vector , which is really a list containing functions to do the following:
 ##	1. set the value of the matrix
 ##	2. get the value of the matrix
@@ -11,7 +11,8 @@
 ##	4. get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-      	i = NULL			 
+## x: the input needs to be a square invertible matrix  
+    	i = NULL			 
 	set = function(y) { 
                 x <<- y
                 i <<- NULL
@@ -26,14 +27,11 @@ makeCacheMatrix <- function(x = matrix()) {
 
 }
 
-
-## Write a short comment describing this function
-
+## Function Description: cacheSolve
 ## The cacheSolve function calculates the inverse of the special matrix created with the makeCache function. 
 ## However, it first checks to see if the inverse has already been calculated. 
-## If the inverse already exists, it gets the mean from the cache and skips the computation. 
+## If the inverse already exists, it gets the inverse from the cache and skips the computation. 
 ## If the inverse does not exist, it calculates the inverse of the matrix and sets the value of the inverse in the cache via the setinverse function.
-
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
